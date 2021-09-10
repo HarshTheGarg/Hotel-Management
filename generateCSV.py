bc8d46eb31392c2ad5916a8c120e35966b676ced
HarshTheGarg <gargharsh2512@gmail.com>
2021-09-11 00:18:44 +0530

    Refactor generateCSV.py file

--- generateCSV.py
+++ generateCSV.py
@@ -1,29 +1,49 @@
 """
-Generates and saves CSV file containing all the customers data on desktop
+Generate and saves CSV file containing all the customers data on desktop
 """
 
-# To Create the Directory
-import os
+import os  # Create the Directory
 
-import csv
+import datetime  # To specify datatype
+
+import csv  # Lib to create csv
 
 
 # To generate CSV file
-def generateCSV(data, date):
-
+def generateCSV(data: list[list], date: datetime.date) -> None:
+    """
+    Generate CSV file and save in the HotelMan directory on desktop
+    :param data:
+    :param date:
+    :rtype: None
+    """
     # Create the folder on desktop
-    desktop_path = os.environ["HOMEPATH"] + "\\Desktop"
-    if os.path.exists("C:" + desktop_path + "\\HotelMan"):
-        pass
-    else:
-        os.mkdir("C:" + desktop_path + "\\HotelMan")
+    desktop_path: str = os.environ["HOMEPATH"] + "\\Desktop"
+    # Path to the desktop
 
-    fields = ["Customer ID", "Name", "Aadhaar", "Mobile", "Room Type", "Check In Date",
-              "Check Out Date", "Check In Status", "Room Rate", "Tax"]
+    # Check if the directory already exists
+    if os.path.exists("C:" + desktop_path + "\\HotelMan"):
+        # Directory exists
+        pass
+
+    else:
+        # Directory doesn't exist
+        os.mkdir("C:" + desktop_path + "\\HotelMan")
+        # Make the directory
+
+    # Create the file
+    fields: list = ["Customer ID", "Name", "Aadhaar", "Mobile", "Room Type", "Check In Date",
+                    "Check Out Date", "Check In Status", "Room Rate", "Tax"]
+    # All the headers to be added to the file
 
     with open("C:" + desktop_path + f"\\HotelMan\\HotelMan_{date.year}_{date.month}_{date.day}.csv",
               "w", newline=""
               ) as file:
         csv_w = csv.writer(file)
+        # CSV writer option
+
         csv_w.writerow(fields)
+        # Write the header row
+
         csv_w.writerows(data)
+        # Write all the data
